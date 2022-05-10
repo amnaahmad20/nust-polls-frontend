4414650aedaf31eb5210616b282fb8dfaae1dcbd
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-05-10 23:59:35 +0500

    feat(Main): include axios request for login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,74 @@
 import React from 'react';
 import Main from './Main';
-import Nav from "./Components/Nav/Nav";
+import Nav from './Components/Nav/Nav';
 import Dashboard from './Components/Dashboard/Dashboard';
-import "./css/App.css"
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import CreatePolls from "./Components/Creation/CreatePolls";
-import ExistingPolls from "./Components/Existing/ExistingPolls";
-
+import './css/App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreatePolls from './Components/Creation/CreatePolls';
+import ExistingPolls from './Components/Existing/ExistingPolls';
 
 function App() {
-
-    return (<div className='App'>
-        <Router>
-            <Routes>
-                <Route path="/" element={<div className='App'>
-                    <Nav/>
-                    <Main/>
-                </div>}>
-                </Route>
-                <Route path="/dash" element={<div className='App'>
-                    <Nav isLoggedIn={true}/>
-                    <Dashboard/>
-                </div>}>
-                </Route>
-                <Route path="/create-poll" element={<div className='App'>
-                    <Nav isLoggedIn={true}/>
-                    <CreatePolls/>
-                </div>}>
-                </Route>
-                <Route path="/view-polls" element={<div className='App'>
-                    <Nav isLoggedIn={true}/>
-                    <ExistingPolls/>
-                </div>}>
-                </Route>
-            </Routes>
-        </Router>
-    </div>);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="App">
+                <Nav />
+                <Main />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={
+              <div className="App">
+                <Nav />
+                <Main />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/student"
+            element={
+              <div className="App">
+                <h1>Welcome</h1>
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/dash"
+            element={
+              <div className="App">
+                <Nav isLoggedIn={true} />
+                <Dashboard />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/create-poll"
+            element={
+              <div className="App">
+                <Nav isLoggedIn={true} />
+                <CreatePolls />
+              </div>
+            }
+          ></Route>
+          <Route
+            path="/view-polls"
+            element={
+              <div className="App">
+                <Nav isLoggedIn={true} />
+                <ExistingPolls />
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,41 +1,87 @@
-import React from 'react'
-import "./css/main.css"
-import eye from "./img/eye.png"
-import {Link, useNavigate} from "react-router-dom";
-
+import React, { useState } from 'react';
+import './css/main.css';
+import eye from './img/eye.png';
+import axios from './axios';
+import { useNavigate } from 'react-router-dom';
 
 function Main() {
-    const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+  const [inputType, setInputType] = useState('password');
+  const navigate = useNavigate();
 
-    return (
-        <section className="sign-in d-flex justify-content-center align-items-center">
-            <div className="sign-in-box text-center mx-4">
-                <h2>Sign in</h2>
-                <form action="{{route('auth.check')}}" method="post">
-                    <input type="email" className="form-control" name='email' placeholder="Enter email" id="email"
-                           value=""/>
-                    <div className="password">
-                        <input type="password" className="form-control" name='password' placeholder="Enter password"
-                               id="pwd"/>
-                        <div className="eye">
-                            <img className="img-fluid" onclick="showPassword()" src={eye} alt="show"/>
-                        </div>
-                    </div>
-                    <button type="submit" onClick={() => navigate("/dash")} class="btn btn-dark dark-button"
-                            component={Link} to="/dash">
-                        Sign In
-                    </button>
-                    <a style={{
-                        display: "block",
-                        cursor: "pointer",
-                        marginTop: "10px",
-                        color: "#063651",
-                        textDecoration: "underline"
-                    }} href='#'>Forgot Password?</a>
-                </form>
+  const showPassword = () => {
+    inputType === 'password' ? setInputType('text') : setInputType('password');
+  };
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('/user/login', { username, password });
+      console.log(res);
+      localStorage.setItem('token', res.data.token);
+      setUsername('');
+      setPassword('');
+      res.data.data.admin ? navigate('/dash') : navigate('/student');
+    } catch (err) {
+      console.log(err.response.data.message);
+    }
+  };
+
+  return (
+    <section className="sign-in d-flex justify-content-center align-items-center">
+      <div className="sign-in-box text-center mx-4">
+        <h2>Sign in</h2>
+        <form onSubmit={loginHandler}>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            placeholder="Enter username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <div className="password">
+            <input
+              type={inputType}
+              className="form-control"
+              name="password"
+              placeholder="Enter password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="eye">
+              <img
+                className="img-fluid"
+                onClick={showPassword}
+                src={eye}
+                alt="show"
+              />
             </div>
-        </section>
-    )
+          </div>
+          <button type="submit" className="btn btn-dark dark-button">
+            Sign In
+          </button>
+          <a
+            style={{
+              display: 'block',
+              cursor: 'pointer',
+              marginTop: '10px',
+              color: '#063651',
+              textDecoration: 'underline',
+            }}
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </form>
+      </div>
+    </section>
+  );
 }
 
-export default Main
+export default Main;
