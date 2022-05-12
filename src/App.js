ae3386c5df02de498294d1af6cefb0b046d85eee
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-05-12 09:39:04 +0500

    fix(App): add location dependency in useEffect

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from './Main';
 import Nav from './Components/Nav/Nav';
 import Dashboard from './Components/Dashboard/Dashboard';
 import './css/App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import CreatePolls from './Components/Creation/CreatePolls';
@@ -15,6 +15,7 @@ import ResetPassword from './Components/Forms/ResetPassword';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [name, setName] = useState('');
+  const location = useLocation();
 
   useEffect(() => {
     if (localStorage.getItem('token')) {
@@ -23,86 +24,84 @@ function App() {
       setIsLoggedIn(true);
       setName(`${decoded.firstName} ${decoded.lastName}`);
     }
-  }, []);
+  }, [location]);
 
   return (
     <div className="App">
       <ToastContainer position="bottom-right" />
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <div className="App">
-                <Nav />
-                <Main />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={
-              <div className="App">
-                <Nav />
-                <Main />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/forgotpassword"
-            element={
-              <div className="App">
-                <Nav />
-                <ForgotPassword />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/resetpassword/:resetToken"
-            element={
-              <div className="App">
-                <Nav />
-                <ResetPassword />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/student"
-            element={
-              <div className="App">
-                <h1>Welcome</h1>
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/dash"
-            element={
-              <div className="App">
-                <Nav isLoggedIn={isLoggedIn} />
-                <Dashboard />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/create-poll"
-            element={
-              <div className="App">
-                <Nav isLoggedIn={isLoggedIn} />
-                <CreatePolls />
-              </div>
-            }
-          ></Route>
-          <Route
-            path="/view-polls"
-            element={
-              <div className="App">
-                <Nav isLoggedIn={isLoggedIn} />
-                <ExistingPolls name={name} />
-              </div>
-            }
-          ></Route>
-        </Routes>
-      </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="App">
+              <Nav />
+              <Main />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/login"
+          element={
+            <div className="App">
+              <Nav />
+              <Main />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/forgotpassword"
+          element={
+            <div className="App">
+              <Nav />
+              <ForgotPassword />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/resetpassword/:resetToken"
+          element={
+            <div className="App">
+              <Nav />
+              <ResetPassword />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/student"
+          element={
+            <div className="App">
+              <h1>Welcome</h1>
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/dash"
+          element={
+            <div className="App">
+              <Nav isLoggedIn={isLoggedIn} />
+              <Dashboard />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/create-poll"
+          element={
+            <div className="App">
+              <Nav isLoggedIn={isLoggedIn} />
+              <CreatePolls />
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/view-polls"
+          element={
+            <div className="App">
+              <Nav isLoggedIn={isLoggedIn} />
+              <ExistingPolls name={name} />
+            </div>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 }
