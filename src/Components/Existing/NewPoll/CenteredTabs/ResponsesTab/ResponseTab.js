77c2e44281cc1e08d9da57db56781ea4c74aede9
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-05-19 23:01:37 +0500

    feat(ResponseTab): display poll responses

--- src/Components/Existing/NewPoll/CenteredTabs/ResponsesTab/ResponseTab.js
+++ src/Components/Existing/NewPoll/CenteredTabs/ResponsesTab/ResponseTab.js
@@ -1,40 +1,166 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ResponseTab.css';
 import DoughnutChart from '../../../../Charts/DoughnutChart';
 import BarChart from '../../../../Charts/BarChart';
 
 function ResponseTab(props) {
+  const [questions, setQuestions] = useState([]);
+  const [responses, setResponses] = useState([]);
+
+  useEffect(() => {
+    console.log('this is responses section');
+    const res = {
+      questions: {
+        _id: '6251893ba5922ce026818b9a',
+        poll: '625183c6a5922ce026818b96',
+        mcq: [
+          {
+            statement: 'Is the hike justified?',
+            index: 0,
+            options: ['Yes', 'No', 'Maybe'],
+          },
+          {
+            statement: 'Were you expecting the hike?',
+            options: ['Yes', 'No'],
+            index: 3,
+          },
+        ],
+        text_based: [
+          {
+            index: 2,
+            statement: 'Question 1',
+          },
+          {
+            statement: 'Question 2',
+            index: 1,
+          },
+          {
+            statement: 'Question 3',
+            index: 4,
+          },
+        ],
+      },
+      responses: {
+        _id: '6283e33598f8283cb8a504ad',
+        poll: '625183c6a5922ce026818b96',
+        text_based: [
+          {
+            index: 2,
+            responses: [
+              {
+                student_id: '6251890f231691cda13f3cea',
+                username: 'fmujahid.bese18seecs',
+                answer: 'Lorem Ipsum is the answer',
+              },
+              {
+                student_id: '62518a328d6cf528028b4381',
+                username: 'fmujahid.bscs20seecs',
+                answer: 'We must get it done',
+              },
+            ],
+          },
+          {
+            index: 1,
+            responses: [],
+          },
+          {
+            index: 4,
+            responses: [],
+          },
+        ],
+        mcq: [
+          {
+            index: 0,
+            responses: [
+              {
+                student_id: '6251890f231691cda13f3cea',
+                username: 'fmujahid.bese18seecs',
+                answer: 0,
+              },
+              {
+                student_id: '62518a328d6cf528028b4381',
+                username: 'fmujahid.bscs20seecs',
+                answer: 1,
+              },
+            ],
+          },
+          {
+            index: 3,
+            responses: [
+              {
+                student_id: '6251890f231691cda13f3cea',
+                username: 'fmujahid.bese18seecs',
+                answer: 1,
+              },
+              {
+                student_id: '62518a328d6cf528028b4381',
+                username: 'fmujahid.bscs20seecs',
+                answer: 0,
+              },
+            ],
+          },
+        ],
+      },
+    };
+    const sortByIndex = (a, b) => {
+      return a.index - b.index;
+    };
+    setQuestions(
+      [...res.questions.mcq, ...res.questions.text_based].sort(sortByIndex)
+    );
+    setResponses(
+      [...res.responses.mcq, ...res.responses.text_based].sort(sortByIndex)
+    );
+    console.log(questions, responses);
+  }, []);
+
+  const calculateCount = (options, qIndex) => {
+    const count = options.map((option, index) =>
+      responses[qIndex].responses.reduce(
+        (acc, cur) => (cur.answer === index ? ++acc : acc),
+        0
+      )
+    );
+    return count;
+  };
+
   return (
     <div className={'responses'}>
-      No responses Yet
-      <div style={{ margin: 'auto', padding: '0 30%' }}>
-        <DoughnutChart
-          labelSet={[
-            'Maybelline',
-            'Huda',
-            'Faiza Beauty Cream',
-            'Miss Rose',
-            'Garnier',
-            'Oriflame',
-            'Rivaj UK',
-            'Other ',
-          ]}
-          dataSet={[100, 100, 100, 100, 100, 100, 100, 100]}
-        />
-        <BarChart
-          labelSet={[
-            'Maybelline',
-            'Huda',
-            'Faiza Beauty Cream',
-            'Miss Rose',
-            'Garnier',
-            'Oriflame',
-            'Rivaj UK',
-            'Other ',
-          ]}
-          dataSet={[20, 100, 60, 70, 100, 80, 65, 40]}
-        />
-      </div>
+      {responses?.length
+        ? questions.map((question, qIndex) => (
+            <div style={{ marginBottom: '30px' }}>
+              <h5>{question.statement}</h5>
+              <p>{`${responses[qIndex].responses.length} responses`}</p>
+              {question.options ? (
+                question.options.length < 4 ? (
+                  <div style={{ margin: 'auto', padding: '0 37.5%' }}>
+                    <DoughnutChart
+                      labelSet={question.options}
+                      dataSet={calculateCount(question.options, qIndex)}
+                    />
+                  </div>
+                ) : (
+                  <div style={{ margin: 'auto', padding: '0 35%' }}>
+                    <BarChart
+                      labelSet={question.options}
+                      dataSet={calculateCount(question.options, qIndex)}
+                    />
+                  </div>
+                )
+              ) : (
+                <div>
+                  {responses[qIndex].responses.map((response) => (
+                    <div>
+                      <p>
+                        {response.answer} {response.username}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))
+        : 'No responses Yet'}
     </div>
   );
 }
